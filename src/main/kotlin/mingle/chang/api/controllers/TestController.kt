3ad3e031a6549480cd8d16c12a7d930200f77ce1
MingleChang <mingle890805@gmail.com>
2019-11-25 12:21:41 +0800

    Update TestController.kt

--- src/main/kotlin/mingle/chang/api/controllers/TestController.kt
+++ src/main/kotlin/mingle/chang/api/controllers/TestController.kt
@@ -1,6 +1,9 @@
 package mingle.chang.api.controllers
 
 import mingle.chang.api.models.Response
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,11 +15,13 @@ class TestController {
     }
     @GetMapping(value=["/.well-known/apple-app-site-association", "/apple-app-site-association"])
     fun appleAppSiteAssociation() : Any {
-        val paths = listOf<String>("/*")
-        val appID = "USZ9R7RQAA.api.minglechang.com"
-        val details = mapOf<String, Any>("appID" to appID, "paths" to paths)
+        val paths = listOf<String>("/iosuniversallink/*")
+        val bundle = mapOf<String, Any>("paths" to paths)
+        val appID = "USZ9R7RQAA.mingle.chang.AppLink"
+        val details = mapOf<String, Any>(appID to bundle)
         val apps = listOf<String>()
         val applinks = mapOf<String, Any>("apps" to apps, "details" to details)
-        return mapOf<String, Any>("applinks" to applinks)
+        val body = mapOf<String, Any>("applinks" to applinks)
+        return  body
     }
 }
